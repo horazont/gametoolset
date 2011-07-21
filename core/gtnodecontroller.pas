08e3e849b156082ce68600753db7833cc5fd8f8f
Jonas Wielicki <j.wielicki@sotecware.net>
2011-07-21 19:04:00 +0200

    * [core/nodes] Prototyped the node controller

--- core/gtnodecontroller.pas
+++ core/gtnodecontroller.pas
@@ -28,14 +28,26 @@ unit GTNodeController;
 interface
 
 uses
-  Classes, SysUtils, GTBase, GTNodes, fgl;
+  Classes, SysUtils, GTBase, GTNodes, fgl, GTXML;
 
 type
-  TGTNodeController = class;
+  TGTServerNodeController = class;
 
   TGTNodeThreadClasses = specialize TFPGList<TGTNodeThreadClass>;
 
-  TGTNodeClassDescriptor = class (TGTBaseObject)
+  { TGTRemoteDescriptor }
+
+  TGTRemoteDescriptor = class (TGTBaseObject)
+  protected
+    FUID: QWord;
+  public
+    procedure LoadFromXML(const XMLNode: TxmlNode; Context: IGTLoaderContext =
+       nil); override;
+  published
+    property UID: QWord read FUID;
+  end;
+
+  TGTNodeClassDescriptor = class (TGTRemoteDescriptor)
   private
     FName, FDescription: String;
   published
@@ -43,7 +55,11 @@ type
     property Description: String read FDescription write FDescription;
   end;
 
-  TGTNodeDataTypeDescriptor = class (TGTBaseObject)
+  { TGTNodeDataTypeDescriptor }
+
+  TGTNodeDataTypeDescriptor = class (TGTRemoteDescriptor)
+  public
+    constructor CreateFromType(const AType: TGTNodeDataTypeClass);
   private
     FName: String;
   published
@@ -52,31 +68,46 @@ type
 
   { TGTNodeDescriptor }
 
-  TGTNodeDescriptor = class (TGTBaseObject)
+  TGTNodeDescriptor = class (TGTRemoteDescriptor)
   public
-    constructor CreateFromNode(const ANode: TGTNode; const AController: TGTNodeController);
+    constructor CreateFromNode(const ANode: TGTNode;
+      const AController: TGTServerNodeController; const RegisterForUpdates: Boolean = True);
   private
-    FID: QWord;
     FInputCount: QWord;
+    FInputNodes: array of TGTNodeDescriptor;
+    FInputTypes: array of TGTNodeDataTypeDescriptor;
     FOutputCount: QWord;
+    FOutputTypes: array of TGTNodeDataTypeDescriptor;
     function GetInputNode(AIndex: Integer): TGTNodeDescriptor;
     function GetInputType(AIndex: Integer): TGTNodeDataTypeDescriptor;
     function GetOutputType(AIndex: Integer): TGTNodeDataTypeDescriptor;
+  protected
+    procedure NodeChanged(Sender: TObject);
+    procedure NodeDeleting(Sender: TObject);
   public
     property InputNode[AIndex: Integer]: TGTNodeDescriptor read GetInputNode;
     property InputTypes[AIndex: Integer]: TGTNodeDataTypeDescriptor read GetInputType;
     property OutputTypes[AIndex: Integer]: TGTNodeDataTypeDescriptor read GetOutputType;
   published
-    property ID: QWord read FID write FID;
     property InputCount: QWord read FInputCount;
     property OutputCount: QWord read FOutputCount;
   end;
+
+  { TGTNodeController }
 
   TGTNodeController = class (TObject)
   public
     constructor Create;
     destructor Destroy; override;
   public
+  //  function Get
+  end;
+
+  { TGTServerNodeController }
+
+  TGTServerNodeController = class (TGTNodeController)
+  public
+    function GetNodeDescriptor(const ANode: TGTNode): TGTNodeDescriptor;
   end;
 
 procedure RegisterNodeThreadClass(const AClass: TGTNodeThreadClass);
@@ -93,28 +124,100 @@ begin
   Registry.Add(AClass);
 end;
 
+{ TGTRemoteDescriptor }
+
+procedure TGTRemoteDescriptor.LoadFromXML(const XMLNode: TxmlNode;
+  Context: IGTLoaderContext);
+var
+  Node: TxmlNode;
+begin
+  inherited LoadFromXML(XMLNode, Context);
+  Node := XMLNode.Node['UID'];
+  if Node <> nil then
+    FUID := StrToQWord(Node.Content);
+end;
+
+{ TGTNodeDataTypeDescriptor }
+
+constructor TGTNodeDataTypeDescriptor.CreateFromType(
+  const AType: TGTNodeDataTypeClass);
+begin
+
+end;
+
 { TGTNodeDescriptor }
 
 constructor TGTNodeDescriptor.CreateFromNode(const ANode: TGTNode;
-  const AController: TGTNodeController);
+  const AController: TGTServerNodeController; const RegisterForUpdates: Boolean
+  );
+var
+  I: Integer;
 begin
   FOutputCount := ANode.PortCount;
   FInputCount := ANode.InPortCount;
+  SetLength(FInputNodes, FInputCount);
+  SetLength(FInputTypes, FInputCount);
+  SetLength(FOutputTypes, FOutputCount);
+  for I := 0 to FInputCount - 1 do
+  begin
+    FInputTypes[I] := TGTNodeDataTypeDescriptor.CreateFromType(ANode.InPort[I].InPipe.DataTypeClass);
+    FInputNodes[I] := AController.GetNodeDescriptor(ANode);
+  end;
+  for I := 0 to FOutputCount - 1 do
+  begin
+    FOutputTypes[I] := TGTNodeDataTypeDescriptor.CreateFromType(TGTNodeDataTypeClass(ANode.Port[I].OutPipe.DataType.ClassType));
+  end;
+
+  if RegisterForUpdates then
+  begin
+    ANode.OnChange.RegisterHandler(@NodeChanged);
+    ANode.OnDestruction.RegisterHandler(@NodeDeleting);
+  end;
 end;
 
 function TGTNodeDescriptor.GetInputNode(AIndex: Integer): TGTNodeDescriptor;
 begin
-
+  Exit(FInputNodes[AIndex]);
 end;
 
 function TGTNodeDescriptor.GetInputType(AIndex: Integer
   ): TGTNodeDataTypeDescriptor;
 begin
-
+  Exit(FInputTypes[AIndex]);
 end;
 
 function TGTNodeDescriptor.GetOutputType(AIndex: Integer
   ): TGTNodeDataTypeDescriptor;
+begin
+  Exit(FOutputTypes[AIndex]);
+end;
+
+procedure TGTNodeDescriptor.NodeChanged(Sender: TObject);
+begin
+
+end;
+
+procedure TGTNodeDescriptor.NodeDeleting(Sender: TObject);
+begin
+  Free;
+end;
+
+{ TGTNodeController }
+
+constructor TGTNodeController.Create;
+begin
+
+end;
+
+destructor TGTNodeController.Destroy;
+begin
+  inherited Destroy;
+end;
+
+{ TGTServerNodeController }
+
+function TGTServerNodeController.GetNodeDescriptor(const ANode: TGTNode
+  ): TGTNodeDescriptor;
 begin
 
 end;
